7844a19069da4ae6f0eed13f6f0d7b7b7842b90b
Giuseppe Roselli <GRoselli@MacBook-Pro-di-Giuseppe.local>
2016-12-05 15:41:50 +0100

    Refactor app to work with local storage

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -37,7 +37,6 @@ export var todosReducer = (state = [], action) => {
         return state.map((todo) => {
           if (todo.id === action.id) {
             var nextCompleted = !todo.completed;
-
             return {
               ...todo,
               completed: nextCompleted,
@@ -47,7 +46,11 @@ export var todosReducer = (state = [], action) => {
             return todo;
           }
         });
-
+      case 'ADD_TODOS':
+        return [
+          ...state,
+          ...action.todos
+        ];
     default:
       return state;
   }
