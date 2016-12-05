f6700958819a7349ce4c0c0292a53cdf9fbdf02c
Giuseppe Roselli <GRoselli@MacBook-Pro-di-Giuseppe.local>
2016-12-05 10:38:04 +0100

    Refactor TodoList, Todo and AddTodo to work with redux

--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -4,27 +4,33 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var AddTodo = require('AddTodo');
+var {AddTodo} = require('AddTodo');
 
 describe('AddTodo', () => {
   it('should exist', () => {
     expect(AddTodo).toExist();
   });
 
-  it('should call onAddTodo prop with valid data', () => {
+  it('should dispatch ADD_TODO when valid todo text', () => {
+    var todoText = 'Check mail';
+    var action = {
+      type: 'ADD_TODO',
+      text: todoText
+    }
     var spy = expect.createSpy();
-    var addTodoForm = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var addTodoForm = TestUtils.renderIntoDocument(<AddTodo dispatch={spy}/>);
     var $el = $(ReactDOM.findDOMNode(addTodoForm));
-
-    addTodoForm.refs.todo.value = 'pippo';
+    console.log('1', todoText);
+    addTodoForm.refs.todo.value = todoText;
+    console.log('addTodoForm.refs.todo.value',addTodoForm.refs.todo.value);
     TestUtils.Simulate.submit($el.find('form')[0]);
-
-    expect(spy).toHaveBeenCalledWith('pippo');
+    console.log(3);
+    expect(spy).toHaveBeenCalledWith(action);
   });
 
-  it('should not call onAddTodo if invalid text entered', () => {
+  it('should not dispatch ADD_TODO when invalid todo text', () => {
     var spy = expect.createSpy();
-    var countdownForm = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var countdownForm = TestUtils.renderIntoDocument(<AddTodo dispatch={spy}/>);
     var $el = $(ReactDOM.findDOMNode(countdownForm));
 
     countdownForm.refs.todo.value = '';
